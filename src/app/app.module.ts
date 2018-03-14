7f3405f5ac1b45df413d6ca63840c62502696b1e
Ali Elderov <elderov@fundsaccess.com>
2018-03-14 19:08:55 +0100

    small refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,42 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LightboxModule } from 'angular2-lightbox';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import {HttpClientModule} from "@angular/common/http";
-import {NgxChartsModule} from "@swimlane/ngx-charts";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {NgxGraphModule} from "@swimlane/ngx-graph";
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { SkillsSvgComponent } from './components/shared/skills-svg/skills-svg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SkillsSvgComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     NgbModule.forRoot(),
     NgxChartsModule,
-    NgxGraphModule
+    NgxGraphModule,
+    LightboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
